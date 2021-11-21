d7cb35a4792caa2af9646f4772be27b7f07fc07a
Fatih Kurt <kurt.fth@gmail.com>
2021-11-21 03:54:01 +0300

    Custom error handler added

---
+++ server/errors.go
@@ -0,0 +1,65 @@
+package server
+
+import (
+	"bytes"
+	"deliveryhero/constants"
+	"errors"
+	"fmt"
+)
+
+type AppError struct {
+	Code    string
+	Message string
+	Op      string
+	Err     error
+}
+
+func ErrorCode(err error) string {
+	if err == nil {
+		return ""
+	}
+	var appError *AppError
+	ok := errors.As(err, &appError)
+
+	if ok && appError.Code != "" {
+		return appError.Code
+	} else if ok && appError.Err != nil {
+		return ErrorCode(appError.Err)
+	}
+	return constants.EINTERNAL
+}
+
+func ErrorMessage(err error) string {
+	if err == nil {
+		return ""
+	}
+	var appError *AppError
+	ok := errors.As(err, &appError)
+	if ok && appError.Message != "" {
+		return appError.Message
+	} else if ok && appError.Err != nil {
+		return ErrorMessage(appError.Err)
+	}
+	return "An internal error has occurred. Please contact technical support."
+}
+
+func (e *AppError) Error() string {
+	var buf bytes.Buffer
+
+	// Print the current operation in our stack, if any.
+	if e.Op != "" {
+		fmt.Fprintf(&buf, "%s: ", e.Op)
+	}
+
+	// If wrapping an error, print its Error() message.
+	// Otherwise print the error code & message.
+	if e.Err != nil {
+		buf.WriteString(e.Err.Error())
+	} else {
+		if e.Code != "" {
+			fmt.Fprintf(&buf, "(%s) ", e.Code)
+		}
+		buf.WriteString(e.Message)
+	}
+	return buf.String()
+}
